d8858c9dc8e5eec4cd06be8cd15b91355f7b439f
toge510 <wr9wu8h8@gmail.com>
2025-01-04 19:18:00 +0900

    Update dns a record

--- google_cloud/terraform/environments/dev-toge510/dns.tf
+++ google_cloud/terraform/environments/dev-toge510/dns.tf
@@ -8,6 +8,6 @@ resource "google_dns_record_set" "dev_toge5103_com_a_record" {
   managed_zone = google_dns_managed_zone.toge5103_com_zone.name
   name         = "dev.toge5103.com."
   type         = "A"
-  rrdatas      = ["10.0.0.1", "10.1.0.1"]
+  rrdatas      = ["35.244.202.26"]
   ttl          = 300
 }
